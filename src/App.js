9111206113fb55e01181f7f837043fec5071c453
ruchika <ruchika6698@gmail.com>
2020-06-07 15:17:13 +0530

    Added:Rounting and Services for history

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route,Redirect } from 'react-router-dom';
+import "./CSS/table.css";
+import { History} from './Components/history'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+    <div>
+      <Router>
+      
+      <Route path="/quantitymeasurement/history" component={History} />
+       
+      </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Services/unitServices.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+ const apiUrl = 'https://localhost:44313/api/measurement';
+
+class Service {
+
+    //API calling for get all history 
+    history() {
+        console.log(" get in axios service ");
+    return   axios.get(apiUrl);
+    }
+    //API calling for delete data by ID
+    delete(Id){
+        console.log(" Id in axios service ",Id);
+    return   axios.delete('https://localhost:44313/api/measurement'+Id);
+    }
+}
+export default Service;
