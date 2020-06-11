15638d4e88412e541cbaa6eaa1e685f124e73b42
ruchika <ruchika6698@gmail.com>
2020-06-11 19:16:43 +0530

    Added:axios method for optiontype

--- src/Services/unitServices.js
+++ src/Services/unitServices.js
@@ -1,18 +1,22 @@
 import axios from 'axios';
 
- const apiUrl = 'https://localhost:44313/api/measurement';
+const apiUrl = 'https://localhost:44313/api/measurement';
 
 class Service {
 
     //API calling for get all history 
     history() {
         console.log(" get in axios service ");
-    return   axios.get(apiUrl);
+    return   axios.get();
     }
     //API calling for delete data by ID
     delete(Id){
         console.log(" Id in axios service ",Id);
-    return   axios.delete('https://localhost:44313/api/measurement/'+Id);
+    return   axios.delete(apiUrl/+Id);
+    }
+    optiontype(data){
+        console.log(" Id in axios service ",data);
+    return   axios.post(apiUrl,data);
     }
 }
 export default Service;
